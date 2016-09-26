4a855c01488724678fb3bafa1badae6956fa275c
skr6 <skr.s@icloud.com>
2016-09-26 19:11:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
    public static void main (String[] par) {
       System.out.println("Hello World");
+      
+      // Sathish added below line
+      System.out.println("GIT demo");
    }   
 }
